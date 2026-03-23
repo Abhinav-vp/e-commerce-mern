155e6204dbdbadc2b67a90eec9c08b694b4ba84d
Abhinav <abhinav.vp@hotmail.com>
2026-03-23 14:52:36 +0530

    hiii

--- src/components/Items/Item.jsx
+++ src/components/Items/Item.jsx
@@ -5,14 +5,26 @@ import { Link } from 'react-router-dom'
 const Item = (props) => {
   return (
     <div className='item'>
-      <Link to={`/product/${props.id}`}><img onClick={window.scrollTo(0,0)} src={props.image} alt="" /></Link>
-      <p>{props.name}</p>
-      <div className="item-prices">
-        <div className="item-price-new">
-            {props.new_price}
+      <Link to={`/product/${props.id}`}>
+        <div className="item-img-container">
+          <img 
+            onClick={() => window.scrollTo(0, 0)} 
+            src={props.image} 
+            alt={props.name} 
+          />
         </div>
-        <div className="item-price-old">
-            {props.old_price}
+      </Link>
+
+      <div className="item-details">
+        <p className="item-name">{props.name}</p>
+
+        <div className="item-prices">
+          <span className="item-price-new">
+            ₹{props.new_price}
+          </span>
+          <span className="item-price-old">
+            ₹{props.old_price}
+          </span>
         </div>
       </div>
     </div>
